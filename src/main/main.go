b74627dd5510261972cbe0eb088c147eca22cfad
Timothy Chung <timchunght@gmail.com>
2015-09-10 02:33:15 -0400

    5-3 done

--- src/main/main.go
+++ src/main/main.go
@@ -4,66 +4,15 @@ import (
   "net/http"
   "os"
   "text/template"
-  "bufio"
-  "strings"
-  "viewmodels"
+  "controllers"
 )
 
 func main() {
 	templates := populateTemplates()
-	
-	http.HandleFunc("/", 
-		func(w http.ResponseWriter, req *http.Request) {
-			requestedFile := req.URL.Path[1:]
-			template :=
-				templates.Lookup(requestedFile + ".html")
-				
-			var context interface{} = nil
-			switch requestedFile {
-			case "home":
-				context = viewmodels.GetHome()
-			case "categories":
-				context = viewmodels.GetCategories()
-			case "products":
-				context = viewmodels.GetProducts()	
-			case "product":
-				context = viewmodels.GetProduct()
-			}
-			if template != nil {
-				template.Execute(w, context)
-			} else {
-				w.WriteHeader(404)
-			}
-		})
-	
-	http.HandleFunc("/img/", serveResource)
-	http.HandleFunc("/css/", serveResource)
-	
-	http.ListenAndServe(":8000", nil)
-}
 
-func serveResource(w http.ResponseWriter, req *http.Request) {
-	path := "public" + req.URL.Path
-	var contentType string
-	if strings.HasSuffix(path, ".css") {
-		contentType = "text/css"
-	} else if strings.HasSuffix(path, ".png") {
-		contentType = "image/png"
-	} else {
-		contentType = "text/plain"
-	}
-	
-	f, err := os.Open(path)
-	
-	if err == nil {
-		defer f.Close()
-		w.Header().Add("Content-Type", contentType)
-		
-		br := bufio.NewReader(f)
-		br.WriteTo(w)
-	} else {
-		w.WriteHeader(404)
-	}
+	controllers.Register(templates)
+
+	http.ListenAndServe(":8000", nil)
 }
 
 func populateTemplates() *template.Template {
